2f2d342d97afcbd05fe26c5b6ca3fbc13154fc48
aturin <aturin@avancore.ru>
2024-02-06 18:07:31 +0300

    Добавил зависимость на сваггер, но она так и не взлетела, как в видео на уроке

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.entity.AppUser;
 import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @RestController
+@Tag(name = "Пользователи", description = "Контроллер для работы с пользователями")
 public class UserController {
     private final UserService userService;
 
